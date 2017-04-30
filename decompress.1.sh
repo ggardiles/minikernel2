a9473fbae48443ce91bba79477dff100a10fa453
Gabriel <ggardiles@gmail.com>
2017-04-30 20:52:16 +0200

    added administration files

---
+++ decompress.1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd /homefi/alumnos/y/y16a042/DATSI/SOA/minikernel.2016; rm -rf autores minikernel/ usuario/ memoria.txt decompress.sh;
+cd /homefi/alumnos/y/y16a042/DATSI/SOA/minikernel.2016; tar -zxvf minikernel.2016.tar.gz;
+
+
+
+
